61d3d6b9087895d92cb0c8d3e7a732c822a1fd1c
雨芩 吳 <48972804+s414june@users.noreply.github.com>
2021-12-20 14:50:40 +0800

    rebuilding site 2021年12月20日 下午 02:50:40

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -d docs # if using a theme, replace with `hugo -t <YOURTHEME>`
 # Add changes to git.
+git remote rm origin
+git remote add origin git@github.com:s414june/xmasGift.git
 git add .
 # Commit changes.
 msg="rebuilding site $(date)"
