78f8099699ee65309bf8926ef050a98a3bece6ab
雨芩 吳 <48972804+s414june@users.noreply.github.com>
2021-12-20 17:51:46 +0800

    rebuilding site 2021年12月20日 下午 05:51:46

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+# If a command fails then the deploy stops
+set -e
+printf "\033[0;32mInit website to GitHub...\033[0m\n"
+#設定連線的網址
 git remote add origin https://github.com/s414june/xmasGift.git
 git fetch origin
 git push -u origin main
@@ -5,3 +10,25 @@ git checkout main
 git add . 
 git commit -m "init github pages"
 git push origin
+#上傳github page
+set -e
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+# Build the project.
+hugo -d docs # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Add changes to git.
+git add .
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+# Push source and build repos.
+git push origin main
+
+#如果還沒設定git，先執行以下指令
+#git config --global user.name <user.name>
+#git config --global user.email <user.email>
+
+#如果還沒設定github帳號密碼存儲，先執行以下指令
+#git config --global credential.helper manager
---
+++ posts/my-first-post.md
@@ -0,0 +1,22 @@
+---
+author: Salt Chang
+title: 使用 Hugo 建立一個 Blog # 文章標題
+subtitle: # 副標題，會在文章頁面顯示
+date: 2020-05-28 # 日期
+description: # 文章描述，不會顯示
+tags: # 標籤，可以分類文章
+banner: # banner 資訊
+  caption:
+  href:
+---
+
+說了好久的部落格，我終於把它建起來了...(淚  
+網域都買了幾個月了，現在才用真的慚愧。  
+就以建這個 Blog 的筆記作為第一篇文吧～
+<!--more-->
+
+---
+
+## 使用 Hugo 建立一個 Blog
+
+<內容...>
